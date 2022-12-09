10c7330cbc96876a40c87d4327c1c37b11e0ee1d
bochkarevatat <tanitabt@rambler.ru>
2022-12-09 19:02:06 +0400

    add task2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Домашнее задание к лекции «TypeScript»
 ## Новые типы
+## Сумма покупки
 ## [![Build status](https://ci.appveyor.com/api/projects/status/k4d0y04qq0667add?svg=true)](https://ci.appveyor.com/project/bochkarevatat/tshomework)
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -10,4 +10,18 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
-}
+
+    getTotalCostExcludingDiscount (): number{
+        return  Array.from(this._items).reduce((sum, current) => sum+current.price, 0)
+    }
+    getTotalCostIncludingDiscount (discount: number): number{
+        return this.getTotalCostExcludingDiscount() * ((100 - discount) / 100)
+    }
+    getDeleteItem(id: number ): void{
+        Array.from(this._items).forEach((item, index) => {
+            if (id === item.id) {
+                this._items.splice(index, 1);
+            }
+          });
+        }
+    }
